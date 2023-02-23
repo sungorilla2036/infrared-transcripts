dbb82783da7126c6aeb3ebf8ede80961280f247d
mrqianhuzi <mrqianhuzi@gmail.com>
2023-02-23 07:24:02 -0500

    Update welcome page, branding, minor fixups

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-import { getCollection } from "astro:content";
-
 export const SITE = {
 	title: 'Infrared Content Catalog',
 	description: 'Unleash the forces of content production',
@@ -8,12 +6,10 @@ export const SITE = {
 
 export const OPEN_GRAPH = {
 	image: {
-		src: 'https://github.com/withastro/astro/blob/main/assets/social/banner-minimal.png?raw=true',
-		alt:
-			'astro logo on a starry expanse of space,' +
-			' with a purple saturn-like planet floating in the right foreground',
+		src: '/images/banner.jpg',
+		alt:''
 	},
-	twitter: 'astrodotbuild',
+	twitter: 'InfraHaz',
 };
 
 export const KNOWN_LANGUAGES = {
@@ -23,7 +19,7 @@ export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
 export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
 
-export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
+export const COMMUNITY_INVITE_URL = `https://discord.gg/JjD5MDSBzk`;
 
 export type Sidebar = Record<
 	(typeof KNOWN_LANGUAGE_CODES)[number],
