db0e8fd73d73eb649f677683a5ea30a94562b263
mrqianhuzi <mrqianhuzi@gmail.com>
2023-06-10 10:14:54 -0400

    f

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,26 @@
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
+
 export default {
-    logo: <span>My Nextra Documentation</span>,
-    project: {
-      link: 'https://github.com/shuding/nextra'
-    }
-    // ...
+  head: () => {
+
+  },
+  logo: <span>Infrared Content Catalog</span>,
+  project: {
+    link: null,
+    icon: null
+  },
+  search: {
+    component: null,
+  },
+  editLink: {
+    component: null,
+  },
+  feedback: {
+    content: null,
+  },
+  navigation: false,
+  components: {
+
   }
+}
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: "react",
+  build: {
+    outDir: "dist",
+  },
+  plugins: [react()],
+  server: {
+    port: 3001,
+  },
+});
