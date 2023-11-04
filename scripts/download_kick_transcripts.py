f125da8773e04306ee1f0192ecfddeca18921688
mrqianhuzi <mrqianhuzi@gmail.com>
2023-11-04 04:00:03 -0400

    chunk via silence removal

--- scripts/download_kick_transcripts.py
+++ scripts/download_kick_transcripts.py
@@ -2,12 +2,14 @@ import subprocess
 from curl_cffi import requests
 import json
 import os
+import numpy as np
+import time
 
 # NeMo model
 # import gc
 # import nemo.collections.asr as nemo_asr
-# from pydub import AudioSegment
-# from pydub.silence import detect_nonsilent
+from pydub import AudioSegment
+from pydub.silence import detect_nonsilent
 
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
@@ -36,32 +38,32 @@ response = requests.get("https://kick.com/api/v1/video/" + uuid,  impersonate="c
 res_json = response.json()
 stream_url = res_json['source']
 print(stream_url)
-print('Downloading stream')
-subprocess.call([
-    'yt-dlp',
-    '-f',
-    'worst',
-    '-x',
-    '--audio-format',
-    'wav',
-    '-o',
-    'temp.wav',
-    stream_url,
-])
+# print('Downloading stream')
+# subprocess.call([
+#     'yt-dlp',
+#     '-f',
+#     'worst',
+#     '-x',
+#     '--audio-format',
+#     'wav',
+#     '-o',
+#     'temp.wav',
+#     stream_url,
+# ])
 
-print('Converting to 16Khz mono wav')
-subprocess.call([
-    'ffmpeg',
-    '-i',
-    'temp.wav',
-    '-ac',
-    '1',
-    '-ar',
-    '16000',
-    uuid + '.wav',
-])
+# print('Converting to 16Khz mono wav')
+# subprocess.call([
+#     'ffmpeg',
+#     '-i',
+#     'temp.wav',
+#     '-ac',
+#     '1',
+#     '-ar',
+#     '16000',
+#     uuid + '.wav',
+# ])
 
-os.remove('temp.wav')
+# os.remove('temp.wav')
 
 def split_on_silence(audio_segment, min_silence_len=1000, silence_thresh=-50, keep_silence=100, max_segment_len=25000):
     """
@@ -92,20 +94,9 @@ def split_on_silence(audio_segment, min_silence_len=1000, silence_thresh=-50, ke
 
     return chunks
 
-# print("Splitting audio...")
-# audio = AudioSegment.from_wav(uuid + ".wav")
-# audio_chunks = split_on_silence(audio, min_silence_len=300, silence_thresh=-40)
-
-# chunk_files = []
-# for i, chunk in enumerate(audio_chunks):
-#     out_file = f"{uuid}_{chunk[1]}_{chunk[2]}.wav"
-#     chunk[0].export(out_file, format="wav")
-#     chunk_files.append(out_file)
-
-# #clear audio_chunks from memory
-# del(audio_chunks)
-# del(audio)
-# gc.collect()
+print("Splitting audio...")
+audio = AudioSegment.from_wav(uuid + ".wav")
+audio_chunks = split_on_silence(audio, min_silence_len=300, silence_thresh=-40)
 
 transcript = []
 print("Running ASR...")
@@ -138,23 +129,20 @@ transcriber = pipeline(
     tokenizer=processor.tokenizer,
     feature_extractor=processor.feature_extractor,
     max_new_tokens=128,
-    chunk_length_s=20,
     torch_dtype=torch_dtype,
     device=device,
 )
 
-result = transcriber(uuid + ".wav", return_timestamps=True)
-for chunk in result['chunks']:
-    chunk['start'] = chunk['timestamp'][0]
-    if chunk['timestamp'][1]:
-        chunk['duration'] = chunk['timestamp'][1] - chunk['timestamp'][0]
-    else:
-        chunk['duration'] = 0
-    del(chunk['timestamp'])
-transcript = result['chunks']
+start_time = time.time()
+for audio_chunk in audio_chunks:
+    result = transcriber(np.array(audio_chunk[0].get_array_of_samples()).T.astype(np.float32)/32768.0)
+    transcript.append(result['text'])
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Elapsed time: {elapsed_time:.2f} seconds")
 
 fout= open(TRANSCRIPTS_FOLDER + uuid + '.json', 'x')
-json.dump(transcript, fout)
+json.dump([{"start": audio_chunks[i][1]/1000, "duration": (audio_chunks[i][2]-audio_chunks[i][1])/1000, "text": text} for i, text in enumerate(transcript)], fout)
 fout.close()
 
 mdx_directory = 'src/content/docs/en/kick/infrared/'
