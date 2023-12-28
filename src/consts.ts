0abc41a594d3d7457d05f2e7fced354836b10553
mrqianhuzi <mrqianhuzi@gmail.com>
2023-12-28 01:22:11 -0500

    Include date in sidebar entries

--- src/consts.ts
+++ src/consts.ts
@@ -1,19 +1,19 @@
 export const SITE = {
-	title: 'Infrared Content Catalog',
-	description: 'Unleash the forces of content production',
-	defaultLanguage: 'en-us',
+  title: "Infrared Content Catalog",
+  description: "Unleash the forces of content production",
+  defaultLanguage: "en-us",
 } as const;
 
 export const OPEN_GRAPH = {
-	image: {
-		src: '/images/banner.jpg',
-		alt:''
-	},
-	twitter: 'InfraHaz',
+  image: {
+    src: "/images/banner.jpg",
+    alt: "",
+  },
+  twitter: "InfraHaz",
 };
 
 export const KNOWN_LANGUAGES = {
-	English: 'en',
+  English: "en",
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
@@ -22,6 +22,6 @@ export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/exa
 export const COMMUNITY_INVITE_URL = `https://discord.gg/JjD5MDSBzk`;
 
 export type Sidebar = Record<
-	(typeof KNOWN_LANGUAGE_CODES)[number],
-	Record<string, { text: string; link: string }[]>
+  (typeof KNOWN_LANGUAGE_CODES)[number],
+  Record<string, { text: string; link: string; date: string }[]>
 >;
